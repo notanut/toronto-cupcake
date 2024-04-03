032ba08af6c6d92692feeab05de9c4f22f841d6e
notanut <natasyajosy@gmail.com>
2024-04-03 14:06:48 +0700

    finished styling and some js

--- js/payment.js
+++ js/payment.js
@@ -13,3 +13,90 @@ function toggleActive(button, id) {
     var div = document.getElementById(id)
     div.classList.remove('none')
 }
+
+
+
+const radioButtons = document.querySelectorAll('input[type="radio"]');
+
+
+// Add click event listeners to each button
+document.getElementById('bcaButton').addEventListener('click', function() {
+    handleRadioButton(this);
+});
+
+document.getElementById('briButton').addEventListener('click', function() {
+    handleRadioButton(this);
+});
+
+document.getElementById('gopayButton').addEventListener('click', function() {
+    handleRadioButton(this);
+});
+
+document.getElementById('gpayButton').addEventListener('click', function() {
+    handleRadioButton(this);
+});
+
+document.getElementById('ovoButton').addEventListener('click', function() {
+    handleRadioButton(this);
+});
+
+document.getElementById('kredivoButton').addEventListener('click', function() {
+    handleRadioButton(this);
+});
+
+document.getElementById('danaButton').addEventListener('click', function() {
+    handleRadioButton(this);
+});
+
+document.getElementById('shopeePayButton').addEventListener('click', function() {
+    handleRadioButton(this);
+});
+
+document.getElementById('bcavButton').addEventListener('click', function() {
+    handleRadioButton(this);
+});
+
+document.getElementById('mandiriButton').addEventListener('click', function() {
+    handleRadioButton(this);
+});
+
+document.getElementById('bniButton').addEventListener('click', function() {
+    handleRadioButton(this);
+});
+
+document.getElementById('brivButton').addEventListener('click', function() {
+    handleRadioButton(this);
+});
+
+document.getElementById('alfaButton').addEventListener('click', function() {
+    handleRadioButton(this);
+});
+document.getElementById('indoButton').addEventListener('click', function() {
+    handleRadioButton(this);
+});
+
+document.getElementById('krediButton').addEventListener('click', function() {
+    handleRadioButton(this);
+});
+document.getElementById('atomeButton').addEventListener('click', function() {
+    handleRadioButton(this);
+});
+
+// Function to handle radio button check
+function handleRadioButton(button) {
+    // Iterate through all radio buttons
+    radioButtons.forEach(function(radioButton) {
+        // Uncheck all radio buttons except for the one associated with the clicked button
+        if (radioButton !== button.querySelector('input[type="radio"]')) {
+            radioButton.checked = false;
+        }
+    });
+    button.querySelector('input[type="radio"]').checked = true;
+    var buttons = document.querySelectorAll('.pay')
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('active');
+    }
+    button.classList.add('active');
+
+    // Check the associated radio button
+}
