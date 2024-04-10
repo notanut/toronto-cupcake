0033de86ecc8b0ec056c1af4d6f0a6413fbec192
novianaretnoo <novianaaretno@gmail.com>
2024-04-10 11:05:06 +0700

    update-footer

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,3 +1,4 @@
+// js buat register
 const inputs = document.querySelectorAll(".input-field");
 const toggle_btn = document.querySelectorAll(".toggle");
 const main = document.querySelector("main");
@@ -38,17 +39,61 @@ bullets.forEach((bullet) => {
   bullet.addEventListener("click", moveSlider);
 });
 
+// js buat navbar
 /*show menu*/
-const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+const showMenu = (toggleId, navId) =>{
+  const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
 
-/*menu */
-navToggle.addEventListener('click', () =>{
-   navMenu.classList.add('show-menu')
+  toggle.addEventListener('click', () =>{
+      // nambah show-menu class ke nav menu
+      nav.classList.toggle('show-menu')
+      // nambah show-icon buat nunjukin sm hide menu icon
+      toggle.classList.toggle('show-icon')
+  })
+}
+
+showMenu('nav-toggle','nav-menu')
+
+/*nampilin dropdown menu*/
+const dropdownItems = document.querySelectorAll('.dropdown_item')
+
+//ngambil semua dropdown item
+dropdownItems.forEach((item) =>{
+   const dropdownButton = item.querySelector('.dropdown_button') 
+
+   //button click
+   dropdownButton.addEventListener('click', () =>{
+       const showDropdown = document.querySelector('.show-dropdown')
+       toggleItem(item)
+       if(showDropdown && showDropdown!== item){
+           toggleItem(showDropdown)
+       }
+   })
 })
 
-/*menu hidden */
-navClose.addEventListener('click', () =>{
-   navMenu.classList.remove('show-menu')
-})
+const toggleItem = (item) =>{
+   const dropdownContainer = item.querySelector('.dropdown_container')
+   if(item.classList.contains('show-dropdown')){
+       dropdownContainer.removeAttribute('style')
+       item.classList.remove('show-dropdown')
+   } else{
+       dropdownContainer.style.height = dropdownContainer.scrollHeight + 'px'
+       item.classList.add('show-dropdown')
+   }
+}
+
+const mediaQuery = matchMedia('(min-width: 1118px)'),
+     dropdownContainer = document.querySelectorAll('.dropdown_container')
+
+const removeStyle = () =>{
+   if(mediaQuery.matches){
+       dropdownContainer.forEach((e) =>{
+           e.removeAttribute('style')
+       })
+       dropdownItems.forEach((e) =>{
+           e.classList.remove('show-dropdown')
+       })
+   }
+}
+addEventListener('resize', removeStyle)
