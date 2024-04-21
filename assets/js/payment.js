e2f57c97c16ac0e7f981c7781a61bc35bd4a630a
notanut <natasyajosy@gmail.com>
2024-04-21 21:45:38 +0700

    add header and footer to payment page

--- assets/js/payment.js
+++ assets/js/payment.js
@@ -1,18 +1,3 @@
-/*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
-
-/* Menu show */
-navToggle.addEventListener('click', () =>{
-   navMenu.classList.add('show-menu')
-})
-
-/* Menu hidden */
-navClose.addEventListener('click', () =>{
-   navMenu.classList.remove('show-menu')
-})
-
 
 var checkName = false
 var checkAddr = false
